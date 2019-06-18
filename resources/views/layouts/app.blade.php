11b265529695ad9b2f283a23139ce967d04fd564
EC2 Default User <ec2-user@ip-172-31-90-17.ec2.internal>
2019-06-18 01:27:42 +0000

    add error and status column

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,10 @@
     <body>
         @include('commons.navbar')
         
+        
         <div class="container">
+            @include('commons.error_messages')
+            
             @yield('content')
         </div>
         
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -9,8 +9,12 @@
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
             
                 <div class="form-group">
-                    {!! Form::label('content', 'メッセージ') !!}
+                    {!! Form::label('content', 'タスク') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                <div class="form-group">
+                    {!! Form::label('status', '進捗') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
                 
                 {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
