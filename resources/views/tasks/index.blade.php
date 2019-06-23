15e7c0d114ba40e729ba2ca391f49c14211bac6b
EC2 Default User <ec2-user@ip-172-31-90-17.ec2.internal>
2019-06-23 06:26:42 +0000

    user login

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <h1>タスク一覧</h1>
+    @if (Auth::check())
+        <h1>タスク一覧</h1>
     
     @if (count($tasks) > 0)
         <table class="table table-striped">
@@ -28,5 +28,12 @@
     {{ $tasks->render('pagination::bootstrap-4') }}
     
     {!! link_to_route('tasks.create', '新規タスク登録', null, ['class' => 'btn btn-primary']) !!}
-
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
+    @endif
 @endsection
